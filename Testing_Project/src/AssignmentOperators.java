be7647ecf177ecff157be6136eaf561ce3bce557
chiru <chithrasiddarajunayak@gmail.com>
2024-10-30 13:29:17 +0530

    new code was added

---
+++ Testing_Project/src/AssignmentOperators.java
@@ -0,0 +1,16 @@
+public class AssignmentOperators{
+        public static void main(String[] args) {
+            int a = 5;
+            a += 3;  // a = a + 3
+            System.out.println(a); // 8
+            a -= 2;  // a = a - 2
+            System.out.println(a); // 6
+            a *= 2;  // a = a * 2
+            System.out.println(a); // 12
+            a /= 3;  // a = a / 3
+            System.out.println(a); // 4
+            a %= 3;  // a = a % 3
+            System.out.println(a); // 1
+        }
+    }
+
