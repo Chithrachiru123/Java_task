318f7753b31b5c03a332d70031cb974baeaf80fa
chiru <chithrasiddarajunayak@gmail.com>
2024-11-03 20:12:22 +0530

    conditional operators was added

---
+++ Testing_Project/src/ConditionalOperator.java
@@ -0,0 +1,9 @@
+/* Write program to find greatest of two numbers using conditional operator */
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        int a = 150;
+        int b = 500;
+        int greater = a>b?a:b;
+        System.out.println(greater);
+    }
+}
